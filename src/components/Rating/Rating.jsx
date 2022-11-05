bb9c8ef56b713c0b50d6a455668607da2915f7d6
Grusl <ffhshdxva@gmail.com>
2022-11-05 15:47:28 +0300

    Added asterisks

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,7 +1,12 @@
+import classnames from "classnames";
+import styles from "../../pages/CatalogPage/styles.module.css";
+
 export const Rating = ({value, max=5, className=0}) => {
     return (
         <>
-            <progress className={ className } value={ value } max={ max } />
+            {Array.apply(0, Array(value)).map(function (x, i) {
+                return '❤';
+            })}
         </>
     )
 }
